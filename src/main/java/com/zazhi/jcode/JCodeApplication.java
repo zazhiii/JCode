04410ef9431abc78045bd27f81ad68034ffef23d
wangnan-1 <w2837509831@gmail.com>
2026-09-03 18:06:55 +0800

    style(ui):添加md包装，美化ui

--- src/main/java/com/zazhi/jcode/JCodeApplication.java
+++ src/main/java/com/zazhi/jcode/JCodeApplication.java
@@ -2,25 +2,15 @@ package com.zazhi.jcode;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author zazhi
  * @date 2026/8/18
- * @description:
  */
 public class JCodeApplication extends Application {
 
@@ -28,7 +18,7 @@ public class JCodeApplication extends Application {
     public void start(Stage stage) throws IOException {
         Agent agent = new Agent();
 
-        Pane pane = (Pane)FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
+        Pane pane = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
 
 //        BorderPane borderPane = new BorderPane();
 //
